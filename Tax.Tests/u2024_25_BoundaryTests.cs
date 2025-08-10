6771baa0753181318e40f0cebf3cbd22310122d3
ManInTheRepo <cusucoland@hotmail.com>
2025-08-10 20:32:21 +1000

    added a new unit test.

---
+++ Tax.Tests/u2024_25_BoundaryTests.cs
@@ -0,0 +1,25 @@
+using Tax.Domain.Schedules;
+using Tax.Domain.Services;
+
+// using Tax.Domain.Services; // <-- uncomment if your calculator lives here
+using Xunit;
+
+namespace Tax.Tests;
+
+public class Au2024_25_BoundaryTests
+{
+    private readonly ProgressiveTaxCalculator _calc = new();
+    
+    [Theory]
+    [InlineData(0,        0)]
+    [InlineData(18_200,   0)]
+    [InlineData(45_000,   4_288)]
+    [InlineData(135_000,  31_288)]
+    [InlineData(190_000,  51_638)]
+    [InlineData(200_000,  56_138)]
+    public void Boundaries_ReturnExpectedTax(decimal income, decimal expectedTax)
+    {
+        var actual = _calc.Calculate(income, Au_2024_25.Brackets);
+        Assert.Equal(expectedTax, decimal.Round(actual, 2));
+    }
+}
